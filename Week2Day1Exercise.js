a33f8a7d3cbed9949467520dffb8e2287630a970
enunes3 <emilynunes95@gmail.com>
2020-11-28 15:29:31 -0500

    Loop Practice

---
+++ Week2Day1Exercise.js
@@ -0,0 +1,16 @@
+/* var age=21;
+if (age>=18) {
+  console.log("Access Granted");
+} else {
+  console.log("Access Denied");
+};
+*/
+
+var num = 9;
+if (num > 0 && num >100) {
+    console.log("Positive and Greater than 100");
+} else if (num >= 0 && num < 100){
+    console.log("Positive and Less than 100");
+} else if (num < 0){
+    console.log("Negative");
+}
