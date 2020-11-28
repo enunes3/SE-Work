8b52429eee24639e7df32de62c61fae64067fd8d
enunes3 <emilynunes95@gmail.com>
2020-11-28 14:37:17 -0500

    Javascript For Looping

---
+++ Week2Day1MorningLab.js
@@ -0,0 +1,27 @@
+
+
+/*
+var num = 9;
+if (num >= 90) {
+    console.log("Grade-A");
+} else if (num >= 80 && num <= 89){
+    console.log("Grade-B");
+} else if (num >=70 && num <= 79){
+    console.log("Grade-C");
+} else if (num >=55 && num <=69) {
+    console.log("Grade-D");
+} else if (num < 55) {
+    console.log("Grade=F");
+}
+*/
+
+var time= 1300;
+if (time >= 0500 && time <= 1159) {
+  console.log('Good Morning!');
+} else if (time >=1200 && time <=1600){
+  console.log('Good Afternoon!');
+} else{
+  console.log('Hey There!');
+}
+
+//when adding anytime during the morning, I keep getting both messages when print
