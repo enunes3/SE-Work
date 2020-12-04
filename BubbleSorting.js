e1fe53f6d50ed0a1f3da2027663f81233a51a925
enunes3 <emilynunes95@gmail.com>
2020-12-04 07:49:10 -0500

    Bubble Sorting
    Javascript Week 3 Day 1

---
+++ BubbleSorting.js
@@ -0,0 +1,19 @@
+//Sort the following #s using the BubbleSort
+let bubbleSort = (Arr) => { //creating the function
+    let len = Arr.length; //creating a length variable
+    let swapped; // creating a variable to keep track of the amount of times the arrays are swapped
+    do {
+        swapped = false; //this is false if there were no swaps made in the array
+        for (let i = 0; i < len; i++) {
+            if (Arr[i] > Arr[i + 1]) { // checks to see if the index [i] is greater than the index to the right
+                let tmp = Arr[i]; // creating a placeholder for the index[i]
+                Arr[i] = Arr[i + 1]; // if [i+1] is true copy the value into the i index
+                Arr[i + 1] = tmp; // copy what was in index i into the index of [i+1] this finishes the swap
+                swapped = true; //the variable swap will be true, which allows the loop to continue
+            }
+        }
+    } while (swapped); //this while loop allows for 1 verification before the code stops. making it more efficient
+    return Arr;
+};
+};
+console.log(bubbleSort([5, 3, 8, 4, 6]));
