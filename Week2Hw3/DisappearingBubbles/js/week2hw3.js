59fe88cf070f9433394c1562e75379cf39b09b90
enunes3 <emilynunes95@gmail.com>
2020-12-07 09:57:42 -0500

    Disappearing Bubbles
    Week 2 Homework 3

---
+++ Week2Hw3/DisappearingBubbles/js/week2hw3.js
@@ -0,0 +1,9 @@
+
+window.addEventListener('click', elements);
+var elements = document.getElementsByClassName("circle");
+function elements(){
+  for (var i=0; i <elements.length; i++) {
+    elements[i].style.backgroundColor = "red";
+  }
+
+}
